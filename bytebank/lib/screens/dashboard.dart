7ecef29a8e3673c48bea6323f8eb7b65da32ac80
Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>
2020-08-29 19:22:43 -0300

    Funcionalidade com bug. No caso esta sendo usado o component ROW que espera um tamanho fixo, e quando ultrapassa da tela acontece o problema. Signed-off-by: Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>

--- bytebank/lib/screens/dashboard.dart
+++ bytebank/lib/screens/dashboard.dart
@@ -21,10 +21,18 @@ class Dashboard extends StatelessWidget {
               _FeatureItem(
                 'Transfer',
                 Icons.monetization_on,
+                onClick: () {
+                  _showContactsList(context);
+                },
               ),
               _FeatureItem(
                 'Transaction Feed',
                 Icons.description,
+                onClick: () => debugPrint('clicado!'),
+              ),_FeatureItem(
+                'Transaction Feed',
+                Icons.description,
+                onClick: () => debugPrint('clicado!'),
               ),
             ],
           ),
@@ -34,11 +42,20 @@ class Dashboard extends StatelessWidget {
   }
 }
 
+void _showContactsList(BuildContext context) {
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) => ContactsList(),
+    ),
+  );
+}
+
 class _FeatureItem extends StatelessWidget {
   final String _name;
   final IconData _icon;
+  final Function onClick;
 
-  _FeatureItem(this._name, this._icon);
+  _FeatureItem(this._name, this._icon, {@required this.onClick});
 
   @override
   Widget build(BuildContext context) {
@@ -47,11 +64,7 @@ class _FeatureItem extends StatelessWidget {
       child: Material(
         color: Theme.of(context).primaryColor,
         child: InkWell(
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => ContactsList(),
-            ));
-          },
+          onTap: () => onClick(),
           child: Container(
             padding: const EdgeInsets.all(8.0),
             height: 100,
