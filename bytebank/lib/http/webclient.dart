1f1de17288ed1f89f0e259600056422282090b02
Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>
2020-09-03 21:09:28 -0300

    Adicionando interceptor na chamada. Signed-off-by: Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>

--- bytebank/lib/http/webclient.dart
+++ bytebank/lib/http/webclient.dart
@@ -1,8 +1,31 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+import 'package:http_interceptor/models/request_data.dart';
 
-void findAll() async{
-  final Response response = await get('http://192.168.99.1:8080/transactions');
-  debugPrint(response.body);
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('Request');
+    print('url ${data.url}');
+    print('headers ${data.headers}');
+    print('body ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print('Response');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+}
+
+void findAll() async {
+  final Client client = HttpClientWithInterceptor.build(
+    interceptors: [LoggingInterceptor()],
+  );
+  final Response response = await client.get('http://192.168.99.1:8080/transactions');
 }
