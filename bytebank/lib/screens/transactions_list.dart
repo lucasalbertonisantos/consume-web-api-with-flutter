d98e6f9fb8fe215775e8103569e7f7a275547109
Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>
2020-09-03 21:35:15 -0300

    Extraindo o progess para um componente. Criando a lista de transações chamando a web api. Inclusão de um Future Delayed para visualizar o progress bar. Signed-off-by: Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>

--- bytebank/lib/screens/transactions_list.dart
+++ bytebank/lib/screens/transactions_list.dart
@@ -1,3 +1,5 @@
+import 'package:bytebank/components/progress.dart';
+import 'package:bytebank/http/webclient.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/models/transaction.dart';
 import 'package:flutter/material.dart';
@@ -7,35 +9,56 @@ class TransactionsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    transactions.add(Transaction(100.0, Contact(0, 'Alex', 1000)));
     return Scaffold(
       appBar: AppBar(
         title: Text('Transactions'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Transaction transaction = transactions[index];
-          return Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text(
-                transaction.value.toString(),
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                transaction.contact.accountNumber.toString(),
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          );
+      body: FutureBuilder<List<Transaction>>(
+        future: Future.delayed(Duration(seconds: 1)).then((value)=>findAll()),
+        builder: (context, snapshot){
+
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+              // TODO: Handle this case.
+              break;
+            case ConnectionState.waiting:
+              return Progress();
+              break;
+            case ConnectionState.active:
+              // TODO: Handle this case.
+              break;
+            case ConnectionState.done:
+              final List<Transaction> transactions = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Transaction transaction = transactions[index];
+                  return Card(
+                    child: ListTile(
+                      leading: Icon(Icons.monetization_on),
+                      title: Text(
+                        transaction.value.toString(),
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        transaction.contact.accountNumber.toString(),
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: transactions.length,
+              );
+              break;
+          }
+          return Text('Unknown error');
         },
-        itemCount: transactions.length,
       ),
+
     );
   }
 }
